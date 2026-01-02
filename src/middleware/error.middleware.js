0a8b70613385d744583ff919a67277c5bbe6da23
rizafrivaldi <rizafrivaldi@gmail.com>
2026-01-02 21:29:44 +0700

    fixing code

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,6 +1,19 @@
 module.exports = (err, req, res, next) => {
-  console.error(err);
+  console.error("GLOBAL ERROR", err);
 
+  if (typeof err === "object" && err.status) {
+    return res.status(err.status).json({
+      success: false,
+      messaage: err.message,
+    });
+  }
+
+  if (err === "FORBIDDEN") {
+    return res.status(403).json({
+      success: false,
+      message: "Forbidden",
+    });
+  }
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Internal server error",
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
