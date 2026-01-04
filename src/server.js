45bec65229f7a8572d31ed8089cbb080cede4ccf
rizafrivaldi <rizafrivaldi@gmail.com>
2026-01-04 22:49:40 +0700

    fixing code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const app = require("./app");
 
-app.use(errorHandler);
-
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
