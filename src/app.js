f119a4b32f91a547bd0d1a166e5a25a72c2faee1
rizafrivaldi <rizafrivaldi@gmail.com>
2026-01-06 23:50:14 +0700

    swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
 const uploadRoutes = require("./routes/uploadRoutes");
 const authRoutes = require("./routes/auth.routes");
 const errorHandler = require("./middleware/error.middleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +15,8 @@ app.unsubscribe("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorHandler);
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
-require("dotenv").config();
 const app = require("./app");
-const errorMiddleware = require("./middleware/error.middleware");
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
