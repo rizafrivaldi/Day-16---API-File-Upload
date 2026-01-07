e1e1688fa42cb54cc90644cbe6d6690b086e986d
rizafrivaldi <rizafrivaldi@gmail.com>
2026-01-07 23:25:18 +0700

    fixing swagger

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,42 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/register", authController.register);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication API
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@mail.com
+ *               password:
+ *                 type: string
+ *                 example: password123
+ *     responses:
+ *       200:
+ *         description: Login success
+ *       401:
+ *         description: Invalid credentials
+ */
 router.post("/login", authController.login);
 
 module.exports = router;
