8f323a0c0d708e0a3bd144c330aa969f6eabbda9
rizafrivaldi <rizafrivaldi@gmail.com>
2026-01-21 23:17:41 +0700

    debug

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "24h" });
 };
 
 exports.verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
